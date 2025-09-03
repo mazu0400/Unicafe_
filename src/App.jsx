65acfb1c6e001e953b44e6acd0426c92a3dbcf96
mazu0400 <matias.tolvanen13@gmail.com>
2025-09-03 12:50:29 +0300

    Unicafe step1

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const total = good + neutral + bad;
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>Statistics</h1>
+      {total === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <div>
+          <p>Good: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
