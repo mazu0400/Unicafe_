84c975e4bbc7f063ced01ce39ce8148908288d87
mazu0400 <matias.tolvanen13@gmail.com>
2025-09-03 12:59:38 +0300

    Unicafe step2

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ const App = () => {
   const [bad, setBad] = useState(0);
 
   const total = good + neutral + bad;
+  const average = total === 0 ? 0 : (good - bad) / total;
+  const positivePercentage = total === 0 ? 0 : (good / total) * 100;
   return (
     <div>
       <h1>Give feedback</h1>
@@ -20,6 +22,9 @@ const App = () => {
           <p>Good: {good}</p>
           <p>Neutral: {neutral}</p>
           <p>Bad: {bad}</p>
+          <p>Total: {total}</p>
+          <p>Average: {average.toFixed(2)}</p>
+          <p>Positive: {positivePercentage.toFixed(2)}%</p>
         </div>
       )}
     </div>
